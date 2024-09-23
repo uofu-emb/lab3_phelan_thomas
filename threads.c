6bac4d1d699db3600ae3f5d033ed9aae898c13f7
Phelan <hobbsphelan@gmail.com>
2024-09-23 12:46:22 -0600

    Added comments and changed to python3

--- threads.c
+++ threads.c
@@ -21,7 +21,9 @@ void side_thread(void *params)
 {
 	while (1) {
         vTaskDelay(100);
+        //count is incremented by side thread
         counter += counter + 1;
+        //print statements are not thread safe
 		printf("hello world from %s! Count %d\n", "thread", counter);
 	}
 }
@@ -31,6 +33,7 @@ void main_thread(void *params)
 	while (1) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         vTaskDelay(100);
+        //print statements are not thread safe
 		printf("hello world from %s! Count %d\n", "main", counter++);
         on = !on;
 	}
@@ -41,11 +44,15 @@ int main(void)
     stdio_init_all();
     hard_assert(cyw43_arch_init() == PICO_OK);
     on = false;
+    //count is not thread safe
     counter = 0;
     TaskHandle_t main, side;
+    //lock is here
     semaphore = xSemaphoreCreateCounting(1, 1);
+    //creates a lock to run main thread without interrupts
     xTaskCreate(main_thread, "MainThread",
                 MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &main);
+    //same, but with side thread so it doesnt share resources
     xTaskCreate(side_thread, "SideThread",
                 SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &side);
     vTaskStartScheduler();
