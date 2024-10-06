636aabeef841a92dfe8bcfa28fefb720403f04b5
thomasjw99 <u1499445@umail.utah.edu>
2024-10-06 13:02:45 -0600

    Removed hellofreertos.c

--- src/threadfunc.c
+++ src/threadfunc.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
 #include <FreeRTOS.h>
 #include <semphr.h>
-#include <task.h>
-#include <pico/stdlib.h>
-#include <pico/multicore.h>
-#include <pico/cyw43_arch.h>
 #include <threadfunc.h>
 
 
@@ -21,4 +16,28 @@ int updateCounter(const char thread_val, int *counter, SemaphoreHandle_t semapho
 
     xSemaphoreGive(semaphore);
     return pdTRUE;
+}
+
+
+void deadlock(void *args)
+{
+    struct DeadlockArgs *dargs = (struct DeadlockArgs *)args;
+
+    printf("\tinside deadlock %c\n", dargs->id);
+    dargs->counter++;
+    xSemaphoreTake(dargs->a, portMAX_DELAY);
+    {
+        dargs->counter++;
+        printf("\tinside first lock %c\n", dargs->id);
+        vTaskDelay(100);
+        printf("\tpost-delay %c\n", dargs->id);
+        xSemaphoreTake(dargs->b, portMAX_DELAY);
+        {
+            printf("\tinside second lock %c\n", dargs->id);
+            dargs->counter++;
+        }
+        xSemaphoreGive(dargs->b);
+    }
+    xSemaphoreGive(dargs->a);
+    vTaskSuspend(NULL);
 }
